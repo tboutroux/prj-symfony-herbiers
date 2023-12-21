1707e588d4cec65701671744d8ece336949d2529
tboutroux <tetetoto@PC-de-TETETOTO.localdomain>
2023-12-21 12:01:32 +0100

    Add Releves CRUD and Releves Forms

---
+++ src/Controller/RelevesController.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Releves;
+use App\Form\Releves1Type;
+use App\Repository\RelevesRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+#[Route('/releves')]
+class RelevesController extends AbstractController
+{
+    #[Route('/', name: 'app_releves_index', methods: ['GET'])]
+    public function index(RelevesRepository $relevesRepository): Response
+    {
+        return $this->render('releves/index.html.twig', [
+            'releves' => $relevesRepository->findAll(),
+        ]);
+    }
+
+    #[Route('/new', name: 'app_releves_new', methods: ['GET', 'POST'])]
+    public function new(Request $request, EntityManagerInterface $entityManager): Response
+    {
+        $relefe = new Releves();
+        $form = $this->createForm(Releves1Type::class, $relefe);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager->persist($relefe);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('app_releves_index', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->render('releves/new.html.twig', [
+            'relefe' => $relefe,
+            'form' => $form,
+        ]);
+    }
+
+    #[Route('/{id}', name: 'app_releves_show', methods: ['GET'])]
+    public function show(Releves $relefe): Response
+    {
+        return $this->render('releves/show.html.twig', [
+            'relefe' => $relefe,
+        ]);
+    }
+
+    #[Route('/{id}/edit', name: 'app_releves_edit', methods: ['GET', 'POST'])]
+    public function edit(Request $request, Releves $relefe, EntityManagerInterface $entityManager): Response
+    {
+        $form = $this->createForm(Releves1Type::class, $relefe);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager->flush();
+
+            return $this->redirectToRoute('app_releves_index', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->render('releves/edit.html.twig', [
+            'relefe' => $relefe,
+            'form' => $form,
+        ]);
+    }
+
+    #[Route('/{id}', name: 'app_releves_delete', methods: ['POST'])]
+    public function delete(Request $request, Releves $relefe, EntityManagerInterface $entityManager): Response
+    {
+        if ($this->isCsrfTokenValid('delete'.$relefe->getId(), $request->request->get('_token'))) {
+            $entityManager->remove($relefe);
+            $entityManager->flush();
+        }
+
+        return $this->redirectToRoute('app_releves_index', [], Response::HTTP_SEE_OTHER);
+    }
+}
---
+++ src/Form/RelevesType.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Lieu;
+use App\Entity\Releves;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class RelevesType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('date')
+            ->add('brut')
+            ->add('lieu_id', EntityType::class, [
+                'class' => Lieu::class,
+                'choice_label' => 'id',
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Releves::class,
+        ]);
+    }
+}
